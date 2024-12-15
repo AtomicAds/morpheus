68ef6363dbbdb0e9d211e9305f4598be243744f2
Tanya Singh <98892932+TSinghh@users.noreply.github.com>
2024-12-15 16:17:44 +0530

    Delete morpheus-main directory

--- morpheus-main/form_builder/forms/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import form_create, form_success
-
-urlpatterns = [
-    path('create/', form_create, name='form_create'),
-    path('success/', form_success, name='form_success'),
-]
